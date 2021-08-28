f2bd742f26130c425fbf50c8af3dd96d9d0e5ce5
abhishek9125 <aagarwal_be17@thapar.edu>
2021-08-29 02:39:34 +0530

    Added: Color Slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Palette from "./components/Palette";
 import seedColors from "./seedColors";
+import {generatePalette} from './helpers/colorHelpers';
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[4]}/>
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
