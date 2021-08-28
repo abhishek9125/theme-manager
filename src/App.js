ae9d5bf537f19f766995aea116392a8b02084c40
abhishek9125 <aagarwal_be17@thapar.edu>
2021-08-29 01:01:27 +0530

    Added: Palletes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Pallete from "./components/Pallete";
 import seedColors from "./seedColors";
 
 function App() {
   return (
     <div className="App">
-      
+      <Pallete />
     </div>
   );
 }
