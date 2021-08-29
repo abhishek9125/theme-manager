b4f81a968b2777f5260ac9feaa41eebf23f46ae8
abhishek9125 <aagarwal_be17@thapar.edu>
2021-08-30 00:26:00 +0530

    Added: Drawer Content

--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
 import classNames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { Drawer, CssBaseline, AppBar, Toolbar, Typography, Divider, IconButton, Button } from "@material-ui/core";
+import { Menu, ChevronLeft } from "@material-ui/icons";
+import { ChromePicker } from "react-color";
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const styles = theme => ({
   root: {
@@ -103,7 +97,7 @@ class NewPaletteForm extends Component {
               onClick={this.handleDrawerOpen}
               className={classNames(classes.menuButton, open && classes.hide)}
             >
-              <MenuIcon />
+              <Menu />
             </IconButton>
             <Typography variant='h6' color='inherit' noWrap>
               Persistent drawer
@@ -121,10 +115,19 @@ class NewPaletteForm extends Component {
         >
           <div className={classes.drawerHeader}>
             <IconButton onClick={this.handleDrawerClose}>
-              <ChevronLeftIcon />
+              <ChevronLeft />
             </IconButton>
           </div>
           <Divider />
+          <Typography variant="h4">
+            Design Your Palette
+          </Typography>
+          <div>
+            <Button variant="contained" color="secondary">Clear Palette</Button>
+            <Button variant="contained" color="primary">Random Color</Button>
+          </div>
+          <ChromePicker color="purple"/>
+          <Button variant="contained" color="primary">Add Color</Button>
         </Drawer>
         <main
           className={classNames(classes.content, {
