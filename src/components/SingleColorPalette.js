8d933add21a0d8898a2541fa65e5d45cfaf1e0af
abhishek9125 <aagarwal_be17@thapar.edu>
2021-08-29 21:22:33 +0530

    Refactored: Color Box Styling

--- src/components/SingleColorPalette.js
+++ src/components/SingleColorPalette.js
@@ -31,7 +31,7 @@ class SingleColorPalette extends Component {
   render() {
     const { format } = this.state;
     const { paletteName, emoji, id } = this.props.palette;
-    const colorBoxes = this._shades.map((color) => <ColorBox key={color.name} name={color.name} background={color[format]} showLink={false}/>)
+    const colorBoxes = this._shades.map((color) => <ColorBox key={color.name} name={color.name} background={color[format]} showingFullPalette={false}/>)
 
     return (
       <div className="SingleColorPalette Palette">
