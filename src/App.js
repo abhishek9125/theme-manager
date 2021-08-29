05c884f8ac56bb024779ace886afd1e9e547b7e7
abhishek9125 <aagarwal_be17@thapar.edu>
2021-08-29 13:22:24 +0530

    Added: Palette Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./components/Palette";
 import seedColors from "./seedColors";
 import {generatePalette} from './helpers/colorHelpers';
 
 function App() {
+
+  function findPalette(id) {
+    return seedColors.find(palette => palette.id === id)
+  }
+
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route 
+        exact 
+        path='/' 
+        render={() => <h1>Main Page</h1>} 
+      />
+      <Route 
+        exact 
+        path='/palette/:id' 
+        render={(routeProps) => <Palette palette={generatePalette(findPalette(routeProps.match.params.id))} />} />
+    </Switch>
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
