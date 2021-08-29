a7e082400e79287d7b3e9b9517937021f47d286d
abhishek9125 <aagarwal_be17@thapar.edu>
2021-08-29 14:35:24 +0530

    Added: Material UI Styling on MiniPalettes

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
+import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
-class PaletteList extends Component {
 
+class PaletteList extends Component {
   getPaletteBox = (palettes) => {
     return palettes.map((palette) => {
       return (
-        <p>
-          <Link to={`/palette/${palette.id}`}>
-            {palette.paletteName}
-          </Link>          
-        </p>
+        <Link to={`/palette/${palette.id}`} style={{ textDecoration: 'none' }}>
+          <MiniPalette {...palette}/>
+        </Link>          
       )
     })
   }
 
   render() {
     const { palettes } = this.props;
-    const paletteBox = palettes.map((palette) => <h1>{palette.paletteName}</h1>)
     return (
       <div className="PaletteList">
+        <MiniPalette />
         <h1>React Colors</h1>
         {this.getPaletteBox(palettes)}
       </div>
